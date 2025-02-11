d9fa2ba4a1eaf87efa7f7253aab78a82b73a7aa5
ZhaoYiwei0311 <zhaoyiwei0311@gmail.com>
2025-02-11 10:02:43 +0800

    Matrix almost Completed

--- src/vector.h
+++ src/vector.h
@@ -12,6 +12,10 @@ typedef struct {
     float z;
 } vec3_t;
 
+typedef struct  {
+    float x, y, z, w;
+} vec4_t;
+
 // Vector 2D functions
 float vec2_length(vec2_t v);
 vec2_t vec2_add(vec2_t a, vec2_t b);
@@ -29,6 +33,10 @@ vec3_t vec3_div(vec3_t v, float factor);
 vec3_t vec3_cross(vec3_t a, vec3_t b);
 float vec3_dot(vec3_t a, vec3_t b);
 void vec3_normalize(vec3_t* v);
+
+// Vector conversion functions
+vec4_t vec4_from_vec3(vec3_t v);
+vec3_t vec3_from_vec4(vec4_t v);
 
 vec3_t vec3_rotate_x(vec3_t v, float angle);
 vec3_t vec3_rotate_y(vec3_t v, float angle);
